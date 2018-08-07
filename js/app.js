698d3eeeb60195e2013cfd1ed62c5b8e36f548b1
randomfederation <randomfederation@users.noreply.github.com>
2018-08-07 18:25:41 +0200

    upd

---
+++ js/app.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    var myVar;
+    var xhr = new XMLHttpRequest();
+    
+    xhr.open("GET", "json/all.json", true);
+    xhr.send();
+
+    xhr.onload = function() {
+            myVar = JSON.parse(xhr.responseText);
+            console.log(myVar);
+            document.getElementById('insideOnload').innerHTML = myvar;
+    };
+
+    xhr.onload();
+
+    document.getElementById('outsideOnload').innerHTML = myVar;
+    console.log(myVar);
+});

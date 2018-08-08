1a116807c5234a2000e9310d367e7b987ac8b7d8
randomfederation <ecigtime@yandex.ru>
2018-08-08 14:39:42 +0100

    upd

--- js/app.js
+++ js/app.js
@@ -1,19 +1,36 @@
 document.addEventListener('DOMContentLoaded', function() {
-
-    var myVar;
+	// var myVar;
+	    	var myVar;
     var xhr = new XMLHttpRequest();
+    // xhr.overrideMimeType("application/json");
     
-    xhr.open("GET", "json/all.json", true);
+    // xhr.onload = function() {
+    // 		console.log(xhr.responseText);
+    //         myVar = JSON.parse(xhr.responseText);
+    //         innerText = "";
+    //         for (var i in myVar) {
+    //         	innerText += myVar[i];
+    //         }
+    //         console.log(myVar);
+    //         document.getElementById('insideOnload').innerHTML = innerText;
+    // };
+
+
+    function getData() {
+
+    	myVar = JSON.parse(xhr.responseText);
+    	console.log(myVar);
+    	innerText = "";
+    	 for (var i in myVar) {
+            	innerText += myVar[i];
+            }
+        document.getElementById('insideOnload').innerHTML = innerText;
+    	return myVar;
+    }
+    xhr.open("GET", "https://www.reddit.com/r/all.json", true);
     xhr.send();
-
-    xhr.onload = function() {
-            myVar = JSON.parse(xhr.responseText);
-            console.log(myVar);
-            document.getElementById('insideOnload').innerHTML = myVar;
-    };
-
-    //xhr.onload();
+    // xhr.onload();
 
     document.getElementById('outsideOnload').innerHTML = myVar;
-    console.log(myVar);
+    console.log("снаружи" + myVar);
 });
